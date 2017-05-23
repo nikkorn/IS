a766df1d164ee3888c0ba21d9f2bc7a38c602727
bencoveney <bencoveney@gmail.com>
2017-05-23 23:00:05 +0100

    Camera System

--- core/src/com/itemshop/game/Game.java
+++ core/src/com/itemshop/game/Game.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.itemshop.input.InputSystem;
+import com.itemshop.render.CameraSystem;
 import com.itemshop.render.RenderSystem;
 import com.itemshop.state.IState;
 import com.itemshop.state.StateSystem;
@@ -29,6 +30,7 @@ public class Game extends ApplicationAdapter {
 		stateSystem.initialize();
 		
 		// Add the other systems.
+		engine.addSystem(new CameraSystem());
 		engine.addSystem(new InputSystem());
 		engine.addSystem(new RenderSystem());
 		
