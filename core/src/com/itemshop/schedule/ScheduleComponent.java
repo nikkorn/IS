9b7dccdd9c387cee6bcadfe2c0f81eb016381e86
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2017-07-17 17:54:28 +0100

    Added schedule activity

--- core/src/com/itemshop/schedule/ScheduleComponent.java
+++ core/src/com/itemshop/schedule/ScheduleComponent.java
@@ -1,7 +1,20 @@
 package com.itemshop.schedule;
 
+import java.util.ArrayList;
 import com.badlogic.ashley.core.Component;
 
 public class ScheduleComponent implements Component {
-
+	
+	/** 
+	 * The list of activities to undertake.
+	 * The first in the list is the currently active one. 
+	 */
+	public ArrayList<Activity> activities;
+	
+	/**
+	 * Create a new instance of the ScheduleComponent class.
+	 */
+	public ScheduleComponent() {
+		activities = new ArrayList<Activity>();
+	}
 }
