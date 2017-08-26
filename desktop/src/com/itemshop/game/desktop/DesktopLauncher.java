300f92258bdc77de53bcb182c5702ce96da7aca3
Ben Coveney <bencoveney@gmail.com>
2017-08-26 01:31:07 +0100

    args, icon, title, rx

--- desktop/src/com/itemshop/game/desktop/DesktopLauncher.java
+++ desktop/src/com/itemshop/game/desktop/DesktopLauncher.java
@@ -1,14 +1,66 @@
 package com.itemshop.game.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.itemshop.game.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		new LwjglApplication(new Game(), buildConfiguration(arg));
+	}
+	
+	private static LwjglApplicationConfiguration buildConfiguration (String[] arguments) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.title = "Item Shoppe";
+		config.addIcon("images/icons/icon-16.png", FileType.Internal);
+		
+		// Define argument defaults.
 		config.width = 1024;
 		config.height = 768;
-		new LwjglApplication(new Game(), config);
+		config.fullscreen = false;
+		config.resizable = true;
+
+		// Process arguments.
+		int current = 0;
+		while (current < arguments.length ) {
+			switch(arguments[current]) {
+				case "-w" :
+				case "--width" :
+					// Could throw "Out of bounds" exception.
+					config.width = Integer.parseInt(arguments[current + 1]);
+					current += 2;
+					break;
+					
+				case "-h" :
+				case "--height" :
+					// Could throw "Out of bounds" exception.
+					config.height = Integer.parseInt(arguments[current + 1]);
+					current += 2;
+					break;
+					
+				case "-f" :
+				case "--fullscreen" :
+					// Prevent resizing when full screen.
+					config.fullscreen = true;
+					config.resizable = false;
+					current += 1;
+					break;
+					
+				case "-r" :
+				case "--resizable" :
+					// Prevent full screen when resizing.
+					config.resizable = true;
+					config.fullscreen = false;
+					current += 1;
+					break;
+					
+				default:
+					throw new IllegalArgumentException("Invalid Command-Line Arguments.");
+			}
+		}
+
+		return config;
 	}
 }
